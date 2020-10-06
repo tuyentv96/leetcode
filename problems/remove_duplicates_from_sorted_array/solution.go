946f83485fc5e53e541e043680138dc2dd7e9b3b
tuyentv96 <vat1906@gmail.com>
2020-10-06 11:48:22 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (golang)

--- problems/remove_duplicates_from_sorted_array/solution.go
+++ problems/remove_duplicates_from_sorted_array/solution.go
@@ -1,17 +1,11 @@
 func removeDuplicates(nums []int) int {
-	if len(nums) < 1 {
-		return 0
-	}
-
-	var prev = nums[len(nums)-1]
-	for i := len(nums) - 2; i >= 0; i-- {
-		if prev == nums[i] {
-			prev = nums[i]
-			nums = append(nums[0:i], nums[i+1:]...)
-			continue
-		}
-
-		prev = nums[i]
-	}
-	return len(nums)
+    var count int
+    for i:=1;i<len(nums);i++{
+        if nums[i]!=nums[count]{
+            count++
+            nums[count], nums[i] = nums[i],nums[count]
+        }
+    }
+    
+    return count+1
 }
