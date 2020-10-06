3035789a130bfa5c0bd3f742c3e19f067840705d
tuyentv96 <vat1906@gmail.com>
2020-10-06 17:57:49 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (golang)

--- problems/best_time_to_buy_and_sell_stock/solution.go
+++ problems/best_time_to_buy_and_sell_stock/solution.go
@@ -1,21 +1,20 @@
 func maxProfit(prices []int) int {
-	if len(prices) <= 1 {
-		return 0
-	}
-
-	var min = prices[0]
-	var max int
-
-	for i := 1; i < len(prices); i++ {
-		diff := prices[i] - min
-		if diff > max {
-			max = diff
-		}
-
-		if prices[i] < min {
-			min = prices[i]
-		}
-	}
-
-	return max
+    if len(prices)<=1{
+        return 0
+    }
+    
+    var res int
+    min:=prices[0]
+    for i:=0;i<len(prices);i++{
+        diff:=prices[i]-min
+        if diff>res{
+            res=diff
+        }
+        
+        if min>prices[i]{
+            min=prices[i]
+        }
+    }
+    
+    return res
 }
