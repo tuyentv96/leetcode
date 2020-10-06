ac41326f5e663048d91171424331d5b71a47f7f8
tuyentv96 <vat1906@gmail.com>
2020-10-06 18:22:29 +0000

    Sync LeetCode submission - House Robber (golang)

--- problems/house_robber/solution.go
+++ problems/house_robber/solution.go
@@ -1,22 +1,30 @@
-func rob(nums []int) int {
-	var prev int
-	var cur int
-	var tmp int
-
-	for i := 0; i < len(nums); i++ {
-		tmp = cur
-		cur = max(cur, prev+nums[i])
-		prev = tmp
-	}
-
-	return cur
+func max(a,b int) int{
+    if a>b{
+        return a
+    }
+    
+    return b
 }
 
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-
-	return b
+func rob(nums []int) int {
+    n:=len(nums)
+    dp:=make([]int,n)
+    
+    if n==0{
+        return 0
+    }
+    
+    if n>0{
+        dp[0]=nums[0]
+    }
+    
+    if n>1{
+        dp[1]=max(dp[0],nums[1])
+    }
+    
+    for i:=2;i<n;i++{
+        dp[i]=max(dp[i-2]+nums[i],dp[i-1])
+    }
+    
+    return dp[n-1]
 }
