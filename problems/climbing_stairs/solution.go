2676b871a671617a2f4ebc12ac30ebb462b8d305
tuyentv96 <vat1906@gmail.com>
2020-10-06 18:04:01 +0000

    Sync LeetCode submission - Climbing Stairs (golang)

--- problems/climbing_stairs/solution.go
+++ problems/climbing_stairs/solution.go
@@ -1,18 +1,23 @@
 func climbStairs(n int) int {
-	dp := make([]int, n)
-	if n <= 1 {
-		return 1
-	}
-	
-	if n == 2 {
-		return 2
-	}
-	dp[0] = 1
-	dp[1] = 2
-
-	for i := 2; i < n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
-	}
-
-	return dp[n-1]
+    if n<1{
+        return 0
+    }
+    
+    if n==1{
+        return 1
+    }
+    
+    if n==2{
+        return 2
+    }
+    
+    var res = 2
+    var prev = 1
+    for i:=3;i<=n;i++{
+        tmp:=res
+        res=res+prev
+        prev=tmp
+    }
+    
+    return res
 }
