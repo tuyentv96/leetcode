ece0b205b93c8700e3df56ffd5d0d16dc5bdae14
tuyentv96 <vat1906@gmail.com>
2021-06-05 18:09:15 +0000

    Sync LeetCode submission - Monotonic Array (golang)

--- problems/monotonic_array/solution.go
+++ problems/monotonic_array/solution.go
@@ -1,14 +1,18 @@
 func isMonotonic(nums []int) bool {
-	inc, desc := true, true
+	var inc, desc bool
 	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] > nums[i+1] {
-			inc=false
+		if nums[i] < nums[i+1] {
+			inc = true
 		}
 
-		if nums[i] < nums[i+1] {
-			desc=false
+		if nums[i] > nums[i+1] {
+			desc = true
 		}
 	}
 
-	return inc || desc
+	if inc && desc {
+		return false
+	}
+
+	return true
 }
