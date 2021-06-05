18baa65ec9207648828805777c39ad418d172bed
tuyentv96 <vat1906@gmail.com>
2021-06-05 18:02:38 +0000

    Sync LeetCode submission - Min Stack (golang)

--- problems/min_stack/solution.go
+++ problems/min_stack/solution.go
@@ -1,48 +1,37 @@
 type MinStack struct {
-	elements, mins []int
-	length         int
+	values, mins []int
+	length       int
 }
 
 /** initialize your data structure here. */
 func Constructor() MinStack {
 	return MinStack{
-		elements: make([]int, 0),
-		mins:     make([]int, 0),
-		length:   0,
+		values: make([]int, 0),
+		mins:   make([]int, 0),
 	}
 }
 
 func (this *MinStack) Push(val int) {
-	this.elements = append(this.elements, val)
-	if this.length == 0 || this.mins[this.length-1] > val {
-		this.mins = append(this.mins, val)
-	} else {
+	this.values = append(this.values, val)
+	if this.length > 0 && this.mins[this.length-1] <= val {
 		this.mins = append(this.mins, this.mins[this.length-1])
+	} else {
+		this.mins = append(this.mins, val)
 	}
 
 	this.length++
 }
 
 func (this *MinStack) Pop() {
-	this.elements = this.elements[:this.length-1]
+	this.values = this.values[:this.length-1]
 	this.mins = this.mins[:this.length-1]
-
 	this.length--
 }
 
 func (this *MinStack) Top() int {
-	return this.elements[this.length-1]
+	return this.values[this.length-1]
 }
 
 func (this *MinStack) GetMin() int {
 	return this.mins[this.length-1]
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Push(val);
- * obj.Pop();
- * param_3 := obj.Top();
- * param_4 := obj.GetMin();
- */
