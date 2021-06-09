50554923667acc57c62216f6312cc16a3dcdfbff
tuyentv96 <vat1906@gmail.com>
2021-06-09 11:25:02 +0000

    Sync LeetCode submission - Add Two Numbers (golang)

--- problems/add_two_numbers/solution.go
+++ problems/add_two_numbers/solution.go
@@ -1,63 +1,32 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var iterator *ListNode
-	var root *ListNode
-	var complement int
+	carry := 0
+	head := &ListNode{}
+	var tail *ListNode
 	for l1 != nil || l2 != nil {
-		newLn := &ListNode{
-			Val:  0,
-			Next: nil,
-		}
-
+		sum := carry
 		if l1 != nil {
-			newLn.Val += l1.Val
-		}
-		if l2 != nil {
-			newLn.Val += l2.Val
-		}
-
-		if complement > 0 {
-			newLn.Val += complement
-			complement = 0
-		}
-
-		if root == nil {
-			root = newLn
-		}
-
-		if iterator == nil {
-			iterator = newLn
-		} else {
-			iterator.Next = newLn
-			iterator = iterator.Next
-		}
-
-		if newLn.Val > 9 {
-			complement = newLn.Val / 10
-			newLn.Val = newLn.Val % 10
-		}
-
-		if l1 != nil {
+			sum += l1.Val
 			l1 = l1.Next
 		}
+
 		if l2 != nil {
+			sum += l2.Val
 			l2 = l2.Next
 		}
-	}
-
-	if complement > 0 && iterator != nil {
-		iterator.Next = &ListNode{
-			Val:  complement,
-			Next: nil,
+		carry = sum / 10
+		p := sum % 10
+		if tail == nil {
+			head.Val = p
+			tail = head
+		} else {
+			tail.Next = &ListNode{Val: p}
+			tail = tail.Next
 		}
-		iterator = iterator.Next
 	}
 
-	return root
+	if carry > 0 {
+		tail.Next = &ListNode{Val: carry}
+	}
+
+	return head
 }
