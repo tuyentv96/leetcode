cd8af91cd69e0ff46486f107b3ec44b9fdb4421f
tuyentv96 <vat1906@gmail.com>
2021-06-09 11:35:58 +0000

    Sync LeetCode submission - Add Strings (golang)

--- problems/add_strings/solution.go
+++ problems/add_strings/solution.go
@@ -1,36 +1,36 @@
 func addStrings(num1 string, num2 string) string {
 	i, j := len(num1)-1, len(num2)-1
-	carry := uint8(0)
-	builder := strings.Builder{}
+	var str strings.Builder
+	var carry int
 	for i >= 0 || j >= 0 {
 		sum := carry
 		if i >= 0 {
-			sum += num1[i] - '0'
+			sum += int(num1[i] - '0')
 		}
 
 		if j >= 0 {
-			sum += num2[j] - '0'
+			sum += int(num2[j] - '0')
 		}
 
+		str.WriteRune(rune((sum % 10) + '0'))
 		carry = sum / 10
-		builder.WriteRune(rune((sum % 10) + '0'))
 
 		i--
 		j--
 	}
 
 	if carry > 0 {
-		builder.WriteRune(rune(carry + '0'))
+		str.WriteRune(rune(carry + '0'))
 	}
 
-	return reverse(builder.String())
+	return reverse(str.String())
 }
 
 func reverse(str string) string {
-	sb := strings.Builder{}
+	strBuilder := strings.Builder{}
 	for i := len(str) - 1; i >= 0; i-- {
-		sb.WriteRune(rune(str[i]))
+		strBuilder.WriteRune(rune(str[i]))
 	}
 
-	return sb.String()
+	return strBuilder.String()
 }
