04d02140c6c2275a08a559a032b759896c3192a5
tuyentv96 <vat1906@gmail.com>
2021-06-10 08:08:53 +0000

    Sync LeetCode submission - Minimum Depth of Binary Tree (golang)

--- problems/minimum_depth_of_binary_tree/solution.go
+++ problems/minimum_depth_of_binary_tree/solution.go
@@ -1,36 +1,27 @@
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
 func minDepth(root *TreeNode) int {
-    if root==nil{
-        return 0
-    }
-    
-    if root.Left ==nil && root.Right==nil{
-        return 1
-    }
-    
-    if root.Left==nil{
-        return 1+minDepth(root.Right)
-    }
-    
-    if root.Right==nil{
-        return 1+minDepth(root.Left)
-    }
-    
-    return 1+min(minDepth(root.Left),minDepth(root.Right))
+	return dfs(root)
 }
 
+func dfs(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
 
-func min(a,b int) int{
-    if a<b{
-        return a
-    }
-    
-    return b
+	if root.Left == nil {
+		return dfs(root.Right) + 1
+	}
+
+	if root.Right == nil {
+		return dfs(root.Left) + 1
+	}
+
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
 }
