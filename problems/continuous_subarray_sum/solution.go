000914c2c417cb7434dee49a65bd8f913b6afb85
tuyentv96 <vat1906@gmail.com>
2021-06-11 14:52:45 +0000

    Sync LeetCode submission - Continuous Subarray Sum (golang)

--- problems/continuous_subarray_sum/solution.go
+++ problems/continuous_subarray_sum/solution.go
@@ -1,21 +1,18 @@
 func checkSubarraySum(nums []int, k int) bool {
+	sum := 0
 	m := make(map[int]int)
-	m[0] = -1
-	r := 0
-	for i := 0; i < len(nums); i++ {
-		r += nums[i]
-		if k != 0 {
-			r = r % k
-		}
-
-		if _, ok := m[r]; ok {
-			if i-m[r] >= 2 {
+    m[0]=-1
+	for i := range nums {
+		sum += nums[i]
+		sum = sum % k
+		if _, ok := m[sum]; ok {
+			if i-m[sum]>=2 {
 				return true
 			}
-		} else {
-			m[r] = i
+		}else {
+			m[sum]=i
 		}
 	}
-
+	
 	return false
 }
