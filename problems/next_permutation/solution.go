19514a9efd4f76231ded092b2d2b19ec2a6314bf
tuyentv96 <vat1906@gmail.com>
2021-07-23 12:04:18 +0000

    Sync LeetCode submission - Next Permutation (golang)

--- problems/next_permutation/solution.go
+++ problems/next_permutation/solution.go
@@ -1,51 +1,31 @@
 func nextPermutation(nums []int) {
-	var j, m int
-	n := len(nums)
-	for i := n - 1; i > 0; i-- {
-		if nums[i-1] < nums[i] {
-			j = i - 1
-			m = i
-			break
-		}
+	i := len(nums) - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
 	}
 
-	if m == 0 && j == 0 {
-		reverse(nums)
-		return
+	if i >= 0 {
+		j := len(nums) - 1
+		for j >= 0 && nums[j] <= nums[i] {
+			j--
+		}
+
+		swap(nums, i, j)
 	}
 
-	for i := n - 1; i > j; i-- {
-		if i == n-1 && j == n-2 {
-			temp := nums[i]
-			nums[i] = nums[j]
-			nums[j] = temp
-			break
-		}
-
-		if i == n-1 && nums[i] > nums[j] {
-			temp := nums[i]
-			nums[i] = nums[j]
-			nums[j] = temp
-			break
-		}
-
-		if nums[j] >= nums[i] && nums[j] < nums[i-1] {
-			temp := nums[i-1]
-			nums[i-1] = nums[j]
-			nums[j] = temp
-			break
-		}
-	}
-
-	reverse(nums[j+1:])
+	reverse(nums, i+1)
 }
 
-func reverse(nums []int) {
-	n := len(nums)
-	for i := 0; i < n/2; i++ {
-		j := n - i - 1
-		temp := nums[i]
-		nums[i] = nums[j]
-		nums[j] = temp
+func swap(nums []int, i, j int) {
+	nums[i], nums[j] = nums[j], nums[i]
+}
+
+func reverse(nums []int, start int) {
+	i := start
+	j := len(nums) - 1
+	for i < j {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
 	}
 }
