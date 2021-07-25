b2b8e29648e5cd58b1ae985ba589f5f752832e69
tuyentv96 <vat1906@gmail.com>
2021-07-25 03:57:48 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (golang)

--- problems/search_in_rotated_sorted_array/solution.go
+++ problems/search_in_rotated_sorted_array/solution.go
@@ -1,7 +1,23 @@
-func search (nums []int, target int) int {
-	for i := range nums {
-		if nums[i] == target {
-			return i
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		m := l + (r-l)/2
+		if nums[m] == target {
+			return m
+		}
+
+		if nums[m] >= nums[l] {
+			if target >= nums[l] && target <= nums[m] {
+				r = m - 1
+			} else {
+				l = m + 1
+			}
+		} else {
+			if target >= nums[m] && target <= nums[r] {
+				l = m + 1
+			} else {
+				r = m - 1
+			}
 		}
 	}
 
