273071c8085c1b291ca0177c30e114c1942e3cbc
tuyentv96 <vat1906@gmail.com>
2021-08-01 00:01:26 +0000

    Sync LeetCode submission - Validate Binary Search Tree (golang)

--- problems/validate_binary_search_tree/solution.go
+++ problems/validate_binary_search_tree/solution.go
@@ -1,33 +1,21 @@
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
 func isValidBST(root *TreeNode) bool {
-    return traversal(root,nil,nil)
+    var prev *TreeNode
+	return dfs(root, &prev)
 }
 
+func dfs(root *TreeNode, prev **TreeNode) bool {
+	if root == nil {
+		return true
+	}
 
-func traversal(root *TreeNode,lower,upper *int) bool{
-    if root==nil{
-        return true
-    }
-    
-    if lower!=nil && root.Val <= *lower{
+    if !dfs(root.Left,prev){
         return false
     }
     
-    if upper!=nil && root.Val >= *upper{
+    if (*prev!=nil && root.Val<=(*prev).Val){
         return false
     }
-
+    *prev=root
     
-    if !traversal(root.Left,lower,&root.Val) || !traversal(root.Right,&root.Val,upper){
-        return false
-    }
-    
-    return true
+    return dfs(root.Right,prev)
 }
