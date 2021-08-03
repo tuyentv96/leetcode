2e3f36a568774a5800a9a9d82289f2f8776dccd6
tuyentv96 <vat1906@gmail.com>
2021-08-03 12:50:00 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (golang)

--- problems/longest_substring_without_repeating_characters/solution.go
+++ problems/longest_substring_without_repeating_characters/solution.go
@@ -2,17 +2,18 @@ func lengthOfLongestSubstring(s string) int {
 	rs := 0
 	start, end := 0, 0
 	var visited [256]bool
-	for end < len(s) {
-		for visited[s[end]] && start <= end {
-			visited[s[start]] = false
-			start++
-		}
-		visited[s[end]] = true
-		rs = max(rs, end-start+1)
-		end++
-	}
-
-	return rs
+    for end<len(s){
+        for visited[s[end]] && start<end{
+            visited[s[start]]=false
+            start++
+        }
+        
+        visited[s[end]]=true
+        rs=max(rs,end-start+1)
+        end++
+    }
+    
+    return rs
 }
 
 func max(a, b int) int {
