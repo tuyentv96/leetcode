5cc41403a08e2f96120910f612113772451112f6
tuyentv96 <vat1906@gmail.com>
2021-08-04 15:49:08 +0000

    Sync LeetCode submission - Permutations (golang)

--- problems/permutations/solution.go
+++ problems/permutations/solution.go
@@ -1,28 +1,29 @@
 func permute(nums []int) [][]int {
-	visited := make([]bool, len(nums))
-	var cur []int
-	var result [][]int
-	dfs(nums, visited, cur, &result)
-	return result
+    var result [][]int
+    var cur []int
+    used:=make(map[int]bool)
+    backtrack(nums,&result,cur,used)
+    return result
 }
 
-func dfs(nums []int, visited []bool, cur []int, result *[][]int) {
-	if len(cur) == len(nums) {
-		c := make([]int, len(cur))
-		copy(c, cur)
-		*result = append(*result, c)
-		return
-	}
 
-	for i := 0; i < len(nums); i++ {
-		if visited[i] {
-			continue
-		}
-
-		visited[i] = true
-		cur = append(cur, nums[i])
-		dfs(nums, visited, cur, result)
-		cur = cur[:len(cur)-1]
-		visited[i] = false
-	}
+func backtrack(nums []int,result *[][]int,cur []int,used map[int]bool){
+    if len(cur)==len(nums){
+        temp := make([]int, len(nums))
+        copy(temp,cur)
+        *result=append(*result,temp)
+        return
+    }
+    
+    for i:=0;i<len(nums);i++{
+        if used[nums[i]]{
+            continue
+        }
+        
+        used[nums[i]]=true
+        cur=append(cur,nums[i])
+        backtrack(nums,result,cur,used)
+        cur=cur[:len(cur)-1]
+        used[nums[i]]=false
+    }
 }
