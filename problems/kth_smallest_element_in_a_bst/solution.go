e9c578f6182c3abd2fde277eb9408645d656694a
tuyentv96 <vat1906@gmail.com>
2021-08-23 12:50:36 +0000

    Sync LeetCode submission - Kth Smallest Element in a BST (golang)

---
+++ problems/kth_smallest_element_in_a_bst/solution.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+    var stack []*TreeNode
+    
+    for true{
+        for root!=nil{
+            stack=append(stack,root)
+            root=root.Left
+        }
+        
+        root=stack[len(stack)-1]
+        stack=stack[:len(stack)-1]
+        k--
+        if k==0{
+            return root.Val
+        }
+        
+        root=root.Right
+    }
+    
+    return 0
+}
