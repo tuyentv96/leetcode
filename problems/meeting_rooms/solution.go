5dc7c6dc12490cc415f2453fad026d464bfac6a5
tuyentv96 <vat1906@gmail.com>
2021-08-24 04:22:35 +0000

    Sync LeetCode submission - Meeting Rooms (golang)

---
+++ problems/meeting_rooms/solution.go
@@ -0,0 +1,13 @@
+func canAttendMeetings(intervals [][]int) bool {
+    sort.Slice(intervals,func(i,j int) bool{
+        return intervals[i][0] < intervals[j][0]
+    })
+    
+    for i:=0;i<len(intervals)-1;i++{
+        if intervals[i][1]>intervals[i+1][0]{
+            return false
+        }
+    }
+    
+    return true
+}
