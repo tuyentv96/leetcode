86b710d908476dc02764a8fbf1f6bcf91850103f
tuyentv96 <vat1906@gmail.com>
2021-08-25 15:58:23 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (golang)

--- problems/kth_largest_element_in_an_array/solution.go
+++ problems/kth_largest_element_in_an_array/solution.go
@@ -1,33 +1,37 @@
 func findKthLargest(nums []int, k int) int {
-	n := len(nums)
-	i := n - k
-	l, r := 0, len(nums)-1
-	for l < r {
-		pivot := partition(nums, l, r)
-		if pivot == i {
-			return nums[pivot]
-		}
-
-		if pivot > i {
-			r = pivot - 1
-		} else {
-			l = pivot + 1
-		}
-	}
-
-	return nums[l]
+    return quickSelect(nums,len(nums)-k)
 }
 
-func partition(nums []int, left, right int) int {
-	pivot := right
-	j := left
-	for i := left; i < right; i++ {
-		if nums[i] < nums[pivot] {
-			nums[i], nums[j] = nums[j], nums[i]
-			j++
-		}
-	}
+func quickSelect(nums []int, i int) int{
+   left,right:=0,len(nums)-1
+    for left<right{
+        pivot:=partition(nums,left,right)
+        if pivot==i{
+            return nums[pivot]
+        }
+        
+        if pivot>i{
+            right=pivot-1
+        }else{
+            left=pivot+1
+        }
+    }
+    
+    return nums[left]
+}
 
-	nums[pivot], nums[j] = nums[j], nums[pivot]
-	return j
+
+func partition(nums []int,left,right int) int{
+    pivot:=right
+    j:=left
+
+    for i:=left;i<right;i++{
+        if nums[i]<nums[pivot]{
+            nums[i],nums[j]=nums[j],nums[i]
+            j++
+        }
+    }
+    
+    nums[pivot],nums[j]=nums[j],nums[pivot]
+    return j
 }
