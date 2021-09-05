8aa9a4f1282ef59d0bb978a3cce935b357d41fc3
tuyentv96 <vat1906@gmail.com>
2021-09-05 04:58:23 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (golang)

--- problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.go
+++ problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.go
@@ -7,30 +7,29 @@
  * }
  */
 func buildTree(preorder []int, inorder []int) *TreeNode {
+    var preorderIndex int
     inorderMap:=make(map[int]int)
+    
     for i:=0;i<len(inorder);i++{
         inorderMap[inorder[i]]=i
     }
     
-    var preorderIndex int
-    return build(inorderMap,preorder,&preorderIndex,0,len(preorder)-1)
+    return build(preorder,&preorderIndex,inorderMap,0,len(preorder)-1)
 }
 
-func build(inorderMap map[int]int,preorder []int,preorderIndex *int,left,right int) *TreeNode{
-    if left>right{
+
+func build(preorder []int,preorderIndex *int,inorderMap map[int]int,l,r int) *TreeNode {
+    if l>r{
         return nil
     }
     
-    rootValue:=preorder[*preorderIndex]
+    root:=&TreeNode{}
+    root.Val = preorder[*preorderIndex]
+    index:=inorderMap[root.Val]
     *preorderIndex++
     
-    var root TreeNode
-    root.Val=rootValue
+    root.Left=build(preorder,preorderIndex,inorderMap,l,index-1)
+    root.Right=build(preorder,preorderIndex,inorderMap,index+1,r)
     
-    rootInorderIndex:=inorderMap[rootValue]
-    
-    root.Left=build(inorderMap,preorder,preorderIndex,left,rootInorderIndex-1)
-    root.Right=build(inorderMap,preorder,preorderIndex,rootInorderIndex+1,right)
-    
-    return &root
+    return root
 }
