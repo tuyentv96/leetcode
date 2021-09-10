cf9878b6957d425e03de38ceef2c1a44e5b3eb54
tuyentv96 <vat1906@gmail.com>
2021-09-10 12:24:51 +0000

    Sync LeetCode submission - Find if Path Exists in Graph (golang)

--- problems/find_if_path_exists_in_graph/solution.go
+++ problems/find_if_path_exists_in_graph/solution.go
@@ -8,23 +8,54 @@ func validPath(n int, edges [][]int, start int, end int) bool {
     }
     
     visited:=make([]bool,n)
-    dfs(graph,visited,start)
-    if visited[end]{
-        return true
+    queue:=NewQueue()
+    queue.Enqueue(start)
+    
+    return bfs(graph,visited,queue,end)
+}
+
+func bfs(graph [][]int,visited []bool,queue Queue,end int) bool{
+    for !queue.IsEmpty(){
+        i:=queue.Dequeue()
+        
+        if i==end{
+            return true
+        }
+        
+        visited[i]=true
+        
+        for j:=0;j<len(graph[i]);j++{
+            k:=graph[i][j]
+            if !visited[k]{
+                queue.Enqueue(k)
+            }
+        }
     }
     
     return false
 }
 
-func dfs(graph [][]int,visited []bool,i int){
-    if visited[i]{
-        return
+type Queue struct{
+    arr []int
+}
+
+func NewQueue() Queue{
+    return Queue{
+        arr: make([]int,0),
     }
-    
-    visited[i]=true
-    for j:=0;j<len(graph[i]);j++{
-        dfs(graph,visited,graph[i][j])
-    }
-    
-    return
+}
+
+func (q *Queue) IsEmpty()bool{
+    return len(q.arr)==0
+}
+
+
+func (q *Queue) Enqueue(i int) {
+    q.arr=append(q.arr,i)
+}
+
+func (q *Queue) Dequeue() int{
+    i:=q.arr[0]
+    q.arr=q.arr[1:]
+    return i
 }
