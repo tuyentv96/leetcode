5beb154427564436a830ee5da76ad984bc505d6f
tuyentv96 <vat1906@gmail.com>
2021-09-10 15:40:26 +0000

    Sync LeetCode submission - All Paths From Source to Target (golang)

--- problems/all_paths_from_source_to_target/solution.go
+++ problems/all_paths_from_source_to_target/solution.go
@@ -1,27 +1,61 @@
+type Queue struct{
+    arr [][]int
+}
+
+func NewQueue() Queue{
+    return Queue{
+        arr: make([][]int,0),
+    }
+}
+
+func (q *Queue) IsEmpty()bool{
+    return len(q.arr)==0
+}
+
+
+func (q *Queue) Enqueue(i []int) {
+    q.arr=append(q.arr,i)
+}
+
+func (q *Queue) Dequeue() []int{
+    i:=q.arr[0]
+    q.arr=q.arr[1:]
+    return i
+}
+
 func allPathsSourceTarget(graph [][]int) [][]int {  
     var result [][]int
-    var tmp []int
     n:=len(graph)
+    path:=[]int{0}
+    queue:=NewQueue()
+    queue.Enqueue(path)
     
-    tmp=append(tmp,0)
-    dfs(graph,&result,tmp,0,n-1)
+    bfs(graph,&result,queue,n-1)
     
     return result
 }
 
-func dfs(graph [][]int,result *[][]int,cur []int,src,dest int) {
-    if src==dest{
-        tmp:=make([]int,len(cur))
-        copy(tmp,cur)
-        *result=append(*result,tmp)
-        return
+func clone(from []int) []int{
+    tmp:=make([]int,len(from))
+    copy(tmp,from)
+    return tmp
+}
+
+func bfs(graph [][]int,result *[][]int,queue Queue,dest int) {
+    for !queue.IsEmpty(){
+        path:=queue.Dequeue()
+        last:=path[len(path)-1]
+        
+        if last==dest{
+            *result=append(*result,clone(path))
+        }
+        
+        for i:=0;i<len(graph[last]);i++{
+            newPath:=clone(path)
+            newPath=append(newPath,graph[last][i])
+            queue.Enqueue(newPath)
+        }
     }
 
-    for i:=0;i<len(graph[src]);i++{
-        cur=append(cur,graph[src][i])
-        dfs(graph,result,cur,graph[src][i],dest)
-        cur=cur[:len(cur)-1]
-    }
-        
     return
 }
