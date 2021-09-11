d46e8b3a9d6b9a3529a34ff36086d72e49f7be9a
tuyentv96 <vat1906@gmail.com>
2021-09-11 19:10:37 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (golang)

--- problems/populating_next_right_pointers_in_each_node/solution.go
+++ problems/populating_next_right_pointers_in_each_node/solution.go
@@ -44,19 +44,3 @@ func connect(root *Node) *Node {
     
     return root
 }
-
-func dfs(cur *Node, next *Node){
-    if cur==nil{
-        return
-    }
-    
-    cur.Next=next
-    dfs(cur.Left,cur.Right)
-    
-    var rightNext *Node
-    if cur.Next!=nil{
-        rightNext=cur.Next.Left
-    }
-    
-    dfs(cur.Right,rightNext)
-}
