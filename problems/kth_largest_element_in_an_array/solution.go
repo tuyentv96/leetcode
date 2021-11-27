8e11fda8f8268bbde43b853e1ad6a814e3fc0359
tuyentv96 <vat1906@gmail.com>
2021-11-27 16:11:05 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (golang)

--- problems/kth_largest_element_in_an_array/solution.go
+++ problems/kth_largest_element_in_an_array/solution.go
@@ -1,37 +1,41 @@
 func findKthLargest(nums []int, k int) int {
-    return quickSelect(nums,len(nums)-k)
+	n := len(nums)
+
+	if k > n {
+		return 0
+	}
+    
+    
+    for i:=k-1;i>=0;i--{
+        heapify(nums,i,k)
+	}
+
+	for i := k; i < n; i++ {
+        if nums[i]>nums[0]{
+            nums[0]=nums[i]
+            heapify(nums,0,k)
+        }
+	}
+
+	return nums[0]
 }
 
-func quickSelect(nums []int, i int) int{
-   left,right:=0,len(nums)-1
-    for left<right{
-        pivot:=partition(nums,left,right)
-        if pivot==i{
-            return nums[pivot]
-        }
-        
-        if pivot>i{
-            right=pivot-1
-        }else{
-            left=pivot+1
-        }
-    }
-    
-    return nums[left]
-}
+// min heap
+func heapify(nums []int, i int, n int) {
+	smallest := i
 
+	left, right := 2*i+1, 2*i+2
 
-func partition(nums []int,left,right int) int{
-    pivot:=right
-    j:=left
+	if left < n && nums[left] < nums[smallest] {
+		smallest = left
+	}
 
-    for i:=left;i<right;i++{
-        if nums[i]<nums[pivot]{
-            nums[i],nums[j]=nums[j],nums[i]
-            j++
-        }
-    }
-    
-    nums[pivot],nums[j]=nums[j],nums[pivot]
-    return j
+	if right < n && nums[right] < nums[smallest] {
+		smallest = right
+	}
+
+	if smallest != i {
+		nums[smallest], nums[i] = nums[i], nums[smallest]
+		heapify(nums, smallest, n)
+	}
 }
