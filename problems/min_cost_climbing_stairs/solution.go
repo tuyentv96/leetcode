80183de1417ea26520691355dcf779ff4dd33e2f
tuyentv96 <vat1906@gmail.com>
2021-12-20 16:04:13 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (golang)

--- problems/min_cost_climbing_stairs/solution.go
+++ problems/min_cost_climbing_stairs/solution.go
@@ -1,14 +1,11 @@
 func minCostClimbingStairs(cost []int) int {
-    n:=len(cost)
-    dp:=make([]int,n)
-    dp[0]=cost[0]
-    dp[1]=cost[1]
+    dp:=make([]int,len(cost)+1)
     
-    for i:=2;i<n;i++{
-        dp[i] = min(dp[i-1],dp[i-2]) + cost[i]
+    for i:=2;i<=len(cost);i++{
+        dp[i]=min(dp[i-2]+cost[i-2],dp[i-1]+cost[i-1])
     }
     
-    return min(dp[n-1],dp[n-2])
+    return dp[len(cost)]
 }
 
 func min(a,b int) int{
