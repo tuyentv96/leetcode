b65aa269072bd3d6e20f0cfb53535c8cfbb77a72
tuyentv96 <vat1906@gmail.com>
2022-01-31 17:25:50 +0000

    Sync LeetCode submission - Coin Change (golang)

--- problems/coin_change/solution.go
+++ problems/coin_change/solution.go
@@ -1,14 +1,15 @@
 func coinChange(coins []int, amount int) int {
+    max:=amount+1
     dp:=make([]int,amount+1)
-    for i:=0;i<len(dp);i++{
-        dp[i]=amount+1
+    
+    for i:=1;i<len(dp);i++{
+        dp[i]=max
     }
     
-    dp[0]=0
     for i:=1;i<=amount;i++{
         for j:=0;j<len(coins);j++{
-            if coins[j]<=i{
-                dp[i]=min(dp[i],dp[i-coins[j]]+1)
+            if coins[j] <= i{
+                dp[i]=min(dp[i],dp[i-coins[j]] + 1)
             }
         }
     }
@@ -27,5 +28,3 @@ func min(a,b int) int{
     
     return b
 }
-
-// 1,2,5
