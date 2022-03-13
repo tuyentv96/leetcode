291120a98e41e022e1c5889996278d0b892638b9
tuyentv96 <vat1906@gmail.com>
2022-03-13 04:33:05 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (python3)

--- problems/longest_increasing_subsequence/solution.py
+++ problems/longest_increasing_subsequence/solution.py
@@ -3,23 +3,20 @@ class Solution:
         sub = [nums[0]]
 
         for num in nums[1:]:
+            i = self.bs_leftmost(sub, num)
             if num > sub[-1]:
                 sub.append(num)
             else:
-                # i = self.bs_leftmost(sub, num)
-                i = bisect.bisect_left(sub, num)
                 sub[i] = num
         return len(sub)
 
     def bs_leftmost(self, nums: List[int], num: int) -> int:
-        lo, hi = 0, len(nums)-1
+        lo, hi = 0, len(nums)
         while lo < hi:
             mid = lo + (hi - lo) // 2
-            if num == mid:
-                hi = mid
-            elif nums[mid] < num:
+            if nums[mid] < num:
                 lo = mid +1
             else:
-                hi = mid -1
+                hi = mid
 
         return lo
