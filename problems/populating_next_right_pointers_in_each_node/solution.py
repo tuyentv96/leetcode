097d652c5129ac71a8c02d915a8b4380254a41c4
tuyentv96 <vat1906@gmail.com>
2022-03-20 10:45:55 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (python3)

---
+++ problems/populating_next_right_pointers_in_each_node/solution.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from queue import Queue
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        
+        leftmost = root
+        while leftmost.left:
+            cur = leftmost
+            
+            while cur:
+                cur.left.next = cur.right
+                if cur.next:
+                    cur.right.next = cur.next.left
+                cur = cur.next
+            
+            leftmost = leftmost.left
+        
+        return root
