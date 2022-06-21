370168fd456dd7a96a8ea142b7ae6d36dd5b6f3f
tuyentv96 <vat1906@gmail.com>
2022-06-21 14:21:21 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (python3)

--- problems/intersection_of_two_arrays_ii/solution.py
+++ problems/intersection_of_two_arrays_ii/solution.py
@@ -3,17 +3,21 @@ class Solution:
         if len(nums1) > len(nums2):
             return self.intersect(nums2, nums1)
 
-        m = dict()
-        for num in nums1:
-            m[num] = m.get(num, 0) + 1
+        nums1.sort()
+        nums2.sort()
         
+        i1, i2 = 0, 0
         k = 0
-        for num in nums2:
-            cnt = m.get(num, 0)
-            if cnt > 0:
-                nums1[k] = num
-                k += 1
-                m[num] = cnt - 1
+        
+        while i1 < len(nums1) and i2 < len(nums2):
+            if nums1[i1] == nums2[i2]:
+                nums1[k] = nums1[i1]
+                k+=1
+                i1+=1
+                i2+=1
+            elif nums1[i1] < nums2[i2]:
+                i1+=1
+            else: i2+=1
         
         return nums1[:k]
         
