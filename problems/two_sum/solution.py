d149a155a026060baceb2aec7866594297ca6c9f
tuyentv96 <vat1906@gmail.com>
2023-12-24 04:50:11 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = dict()
-        result = []
-        for i, num in enumerate(nums):
-            if (target - num) in seen:
-                result.extend((i, seen[target - num]))
+        arr = {}
 
-            if num not in seen:
-                seen[num]=i
+        for idx, num in enumerate(nums):
+            if (target-num) in arr:
+                return [arr[target-num], idx] 
+            
+            arr[num] = idx
         
-        return result
+        return []
+        
