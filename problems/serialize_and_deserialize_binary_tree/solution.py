8f28528be78176ea29e4ed95a45e237a46789fda
tuyentv96 <vat1906@gmail.com>
2024-01-01 05:46:46 +0000

    Sync LeetCode submission - Serialize and Deserialize Binary Tree (python3)

--- problems/serialize_and_deserialize_binary_tree/solution.py
+++ problems/serialize_and_deserialize_binary_tree/solution.py
@@ -4,23 +4,27 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
 
 class Codec:
+
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
-        def helper(root, string):
-            if not root:
-                string += 'None,'
-            else:
-                string += str(root.val) + ','
-                string = helper(root.left, string)
-                string = helper(root.right, string)
-            return string
-        return helper(root, '')
+        res = []
+        def dfs(node):
+            if not node:
+                res.append("null")
+                return
+
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(res)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -28,26 +32,19 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        def helper(l):
-            if len(l) == 0:
+        vals = data.split(",")
+        self.i = 0
+        def dfs(vals):
+            if vals[self.i] == 'null':
+                self.i += 1
                 return None
-            if l[0] == 'None':
-                l.pop(0)
-                return None
-            root = TreeNode(l[0])
-            l.pop(0)
-            root.left = helper(l)
-            root.right = helper(l)
-            return root
 
-        arr = data.split(',')
-        if len(arr) == 0:
-            return None
-
-        root = helper(arr)
-        return root
-        
-        
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left=dfs(vals)
+            node.right=dfs(vals)
+            return node
+        return dfs(vals)
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
