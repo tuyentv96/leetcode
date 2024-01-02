5a12d0479edeff57825870a32ae2e8c83c307bbc
tuyentv96 <vat1906@gmail.com>
2024-01-02 18:31:24 +0000

    Sync LeetCode submission - Merge k Sorted Lists (python3)

--- problems/merge_k_sorted_lists/solution.py
+++ problems/merge_k_sorted_lists/solution.py
@@ -4,33 +4,23 @@ from heapq import *
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
+    ListNode.__eq__ = lambda self, other: self.val == other.val
+    ListNode.__lt__ = lambda self, other: self.val < other.val
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         heap = []
-        queue = deque()
-        root = None
-        cur = None
-        prev = None
-
-        for l in lists:
-            queue.appendleft(l)
-
-        while len(queue) > 0 or len(heap) > 0:
-            prev = cur
-            for i in range(len(queue)):
-                l = queue.pop()
-                if l:
-                    heappush(heap, l.val)
-                    if l.next:
-                        queue.appendleft(l.next)
-            
-            if heap:
-                val = heappop(heap)
-
-                cur = ListNode(val)
-                if not root:
-                    root = cur
-                if prev:
-                    prev.next = cur
-            
-        return root
+        root = ListNode(None)
+        cur = root
+        
+        for node in lists:
+            if node:
+                heappush(heap, (node.val, node))
+        
+        while len(heap) > 0:
+            cur.next = heappop(heap)[1]
+            cur = cur.next
+            if cur.next:
+                heappush(heap, (cur.next.val, cur.next))
+        
+        return root.next
