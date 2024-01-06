a671f0b560ba77cd195dad37417824f573b6a412
tuyentv96 <vat1906@gmail.com>
2024-01-06 09:24:39 +0000

    Sync LeetCode submission - Pacific Atlantic Water Flow (python3)

--- problems/pacific_atlantic_water_flow/solution.py
+++ problems/pacific_atlantic_water_flow/solution.py
@@ -1,26 +1,27 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        visited_p = set()
+        visited_a = set()
+
         ROWS = len(heights)
         COLS = len(heights[0])
-        visited_pac = set()
-        visited_atl = set()
 
-        def dfs(r, c, visited, prev_height):
-            if c < 0 or r < 0 or c >= COLS or r >= ROWS or (r,c) in visited or heights[r][c] < prev_height:
+        def dfs(r, c, visited, prev):
+            if c < 0 or r < 0 or c >= COLS or r >= ROWS or (r, c) in visited or heights[r][c] < prev:
                 return
 
             visited.add((r, c))
-            dfs(r+1, c, visited, heights[r][c])
-            dfs(r-1, c, visited, heights[r][c])
-            dfs(r, c+1, visited, heights[r][c])
-            dfs(r, c-1, visited, heights[r][c])
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
 
-        for c in range(COLS):
-            dfs(0, c, visited_pac, heights[0][c])
-            dfs(ROWS-1, c, visited_atl, heights[ROWS-1][c])
+        for i in range(ROWS):
+            dfs(i, 0, visited_p, heights[i][0])
+            dfs(i, COLS - 1, visited_a, heights[i][COLS - 1])
 
-        for r in range(ROWS):
-            dfs(r, 0, visited_pac, heights[r][0])
-            dfs(r, COLS-1, visited_atl, heights[r][COLS-1])
-
-        return list(visited_pac.intersection(visited_atl))
+        for i in range(COLS):
+            dfs(0, i, visited_p, heights[0][i])
+            dfs(ROWS - 1, i, visited_a, heights[ROWS - 1][i]) 
+            
+        return list(visited_p.intersection(visited_a))
