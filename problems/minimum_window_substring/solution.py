0243fa668d9b067e55c98cecc986a70c7333e6e9
tuyentv96 <vat1906@gmail.com>
2024-01-06 16:29:46 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- problems/minimum_window_substring/solution.py
+++ problems/minimum_window_substring/solution.py
@@ -1,34 +1,32 @@
+# ADOBECODEBANC
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         have = 0
-        need = 0
-        res = None
-        count_s = {}
-        count_t = {}
-        min_size = float('Infinity')
+        s_count = {}
+        t_count = {}
 
         for c in t:
-            count_t[c] = count_t.get(c, 0) + 1
-        
-        need = len(count_t)
+            t_count[c] = t_count.get(c, 0) + 1
+
         l = 0
+        res = None
+        min_size = float('inf')
+        need = len(t_count)
         for r in range(len(s)):
             c = s[r]
-            count_s[c] = count_s.get(c, 0) + 1
-            if count_s[c] == count_t.get(c, 0):
+            s_count[c] = s_count.get(c, 0) + 1
+            if s_count[c] == t_count.get(c, 0):
                 have += 1
-    
+            
             while have == need:
                 if r - l < min_size:
-                    min_size = r - l
                     res = (l, r)
+                    min_size = r - l
 
-                count_s[s[l]] -= 1
-                if count_s[s[l]] < count_t.get(s[l], 0):
+                s_count[s[l]] -= 1
+                if s_count[s[l]] < t_count.get(s[l], 0):
                     have -= 1
-
                 l += 1
-            
 
-        return s[res[0]:res[1]+1] if res else ""
-                
+        return s[res[0]:res[1] + 1] if res else ""
