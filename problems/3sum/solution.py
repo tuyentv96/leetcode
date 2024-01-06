34e3553e1157ad5e07d4436ef219e5727c0bc525
tuyentv96 <vat1906@gmail.com>
2024-01-06 06:08:08 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -1,28 +1,27 @@
+# -1,0,1,2,-1,-4
+# -1,-1,0,1,2,4
+# i = -1 => l=-1,r=0; 
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        sorted_nums = sorted(nums)
-        length = len(nums)
-        print(sorted_nums)
-
-        result = []
-        for i in range(length):
-            left = i + 1
-            right = length - 1
-
-            if i > 0 and sorted_nums[i] == sorted_nums[i - 1]:
+        nums.sort()
+        res = []
+        n = len(nums)
+        for i in range(n):
+            l, r = i + 1, n - 1
+            target = 0 - nums[i]
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-
-            while left < right:
-                sum = sorted_nums[i] + sorted_nums[left] + sorted_nums[right]
-                if sum == 0:
-                    result.append([sorted_nums[i], sorted_nums[left], sorted_nums[right]])
-
-                    while left < right and sorted_nums[left] == sorted_nums[left + 1]:
-                        left += 1
-                    left += 1
-                elif sum < 0:
-                    left += 1
+                
+            while l < r:
+                if nums[l] + nums[r] == target:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1                  
+                    while l < r and nums[l-1] == nums[l]:
+                        l += 1  
+                elif nums[l] + nums[r] < target:
+                    l += 1
                 else:
-                    right -= 1
+                    r -= 1
 
-        return result
+        return res
