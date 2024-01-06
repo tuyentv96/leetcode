40bb23f775f5eebc8aefa64723a8298a7added6e
tuyentv96 <vat1906@gmail.com>
2024-01-06 03:12:40 +0000

    Sync LeetCode submission - Merge Intervals (python3)

--- problems/merge_intervals/solution.py
+++ problems/merge_intervals/solution.py
@@ -1,20 +1,11 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals) < 2:
-            return intervals
-
-        result = []
-        intervals.sort(key=lambda interval: interval[0])
-        begin, end = intervals[0][0], intervals[0][1]
+        intervals.sort()
+        res = [intervals[0]]
 
         for i in range(1, len(intervals)):
-            if end < intervals[i][0]:
-                result.append([begin, end])
-                begin = intervals[i][0]
-                end = intervals[i][1]
+            if intervals[i][0] <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
             else:
-                end = max(end, intervals[i][1])
-        
-        result.append([begin, end])
-
-        return result
+                res.append(intervals[i])
+        return res
