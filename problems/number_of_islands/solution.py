ce1476320b099f03b2e31133061fb506fab20e2c
tuyentv96 <vat1906@gmail.com>
2024-01-07 02:23:51 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,25 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        res = 0
-
-        def dfs(grid: List[List[str]], i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= ROWS or j >= COLS or grid[i][j] == '0':
                 return
+            grid[i][j] = '0'
 
-            if grid[i][j] == '1':
-                grid[i][j] = 'x'
-                dfs(grid, i + 1, j)
-                dfs(grid, i - 1, j)
-                dfs(grid, i, j + 1)
-                dfs(grid, i, j - 1)
-
-
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        
+        res = 0
+        for i in range(ROWS):
+            for j in range(COLS):
                 if grid[i][j] == '1':
                     res += 1
-                    dfs(grid, i, j)
-        
+                    dfs(i, j)
         return res
-    
+         
