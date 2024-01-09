27c37081d659b096763f8b4548bcfe29b76b901e
tuyentv96 <vat1906@gmail.com>
2024-01-09 15:36:50 +0000

    Sync LeetCode submission - Course Schedule (python3)

--- problems/course_schedule/solution.py
+++ problems/course_schedule/solution.py
@@ -1,30 +1,28 @@
+from collections import defaultdict
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = {}
+        graph = defaultdict(list)
         for p in prerequisites:
-            pre, course = p[0], p[1]
-            if course not in graph:
-                graph[course] = []
-            graph[course].append(pre)
-
-        visited = set()
-        courses = {}
-
-        for course in range(numCourses):
-            if not self.dfs(course, graph, visited, courses):
-                return False
-
-        return True
+            graph[p[1]].append(p[0])
         
-    def dfs(self, val, graph, visited, courses):
-        if val in visited:
-            return courses.get(val, False)
-
-        visited.add(val)
-        for v in graph.get(val, []):
-            if not self.dfs(v, graph, visited, courses):
-                courses[v] = False
+        visited = [False] * numCourses
+        current_path = [False] * numCourses
+        for i in range(numCourses):
+            if not visited[i] and self.is_cycle(graph, visited, current_path, i):
                 return False
-        
-        courses[val] = True
         return True
+
+    def is_cycle(self, graph, visited, current_path, i):
+        if visited[i]:
+            return current_path[i]
+
+        visited[i] = True
+        current_path[i] = True
+
+        for j in graph[i]:
+            if self.is_cycle(graph, visited, current_path, j):
+                return True
+        
+        current_path[i] = False
+        return False
