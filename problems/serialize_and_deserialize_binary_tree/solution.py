2361fe03e5e78a5fdcb7ed5dbd85d777e0aba0a4
tuyentv96 <vat1906@gmail.com>
2024-01-09 17:26:05 +0000

    Sync LeetCode submission - Serialize and Deserialize Binary Tree (python3)

--- problems/serialize_and_deserialize_binary_tree/solution.py
+++ problems/serialize_and_deserialize_binary_tree/solution.py
@@ -4,8 +4,6 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-from collections import deque
-
 class Codec:
 
     def serialize(self, root):
@@ -17,14 +15,15 @@ class Codec:
         res = []
         def dfs(node):
             if not node:
-                res.append("null")
+                res.append('null')
                 return
-
+            
             res.append(str(node.val))
             dfs(node.left)
             dfs(node.right)
+        
         dfs(root)
-        return ",".join(res)
+        return ','.join(res)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -32,19 +31,22 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        vals = data.split(",")
+        arr = data.split(',')
+        print(arr)
         self.i = 0
-        def dfs(vals):
-            if vals[self.i] == 'null':
+        def dfs():
+            if arr[self.i] == 'null':
                 self.i += 1
                 return None
-
-            node = TreeNode(int(vals[self.i]))
+            
+            node = TreeNode(arr[self.i])
             self.i += 1
-            node.left=dfs(vals)
-            node.right=dfs(vals)
+            node.left = dfs()
+            node.right = dfs()
             return node
-        return dfs(vals)
+        return dfs()
+
+        
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
