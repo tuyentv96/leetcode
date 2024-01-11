0c88fb5938d38287aaa37ec91af5f585baf9d8ba
tuyentv96 <vat1906@gmail.com>
2024-01-11 02:23:44 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- problems/top_k_frequent_elements/solution.py
+++ problems/top_k_frequent_elements/solution.py
@@ -1,34 +1,37 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
-        for num in nums:
-            count[num] = count.get(num, 0) + 1
-
+        for i in nums:
+            count[i] = count.get(i, 0) + 1
+        
         counts = list(count.items())
         n = len(counts)
-        self.quick_select(counts, n-k, 0, n-1)
-        return [num for (num, c) in counts[n-k:]]
-
-    def partition(self, nums, left, right):
-        pivot = right
-        j = left
-        for i in range(left, right):
-            if nums[i][1] < nums[pivot][1]:
-                nums[i], nums[j] = nums[j], nums[i]
-                j += 1
         
-        nums[pivot], nums[j] = nums[j], nums[pivot]
-        return j
-
-    def quick_select(self, nums, index, left, right):
-        if left > right:
+        pivot = self.quick_select(0, n - 1, n - k, counts)
+        return [num for (num, count) in counts[n-k:]]
+        
+    def quick_select(self, l, r, target_idx, counts):
+        print(l, r, target_idx)
+        if l > r:
             return
         
-        pivot = self.partition(nums, left, right)
-        if pivot == index:
+        pivot_idx = self.partition(l, r, counts)
+        if target_idx == pivot_idx:
             return
-        elif pivot < index:
-            self.quick_select(nums, index, pivot + 1, right)
+        elif pivot_idx < target_idx:
+            self.quick_select(pivot_idx + 1, r, target_idx, counts)
         else:
-            self.quick_select(nums, index, left, pivot - 1)
-  
+            self.quick_select(l, pivot_idx - 1, target_idx, counts)
+
+        
+    def partition(self, l, r, counts):
+        pivot = r
+        j = l
+        for i in range(l, r):
+            if counts[i][1] < counts[pivot][1]:
+                counts[i], counts[j] = counts[j], counts[i]
+                j += 1
+        print(counts)
+        counts[pivot], counts[j] = counts[j], counts[pivot]
+        print(r, j, counts)
+        return j
